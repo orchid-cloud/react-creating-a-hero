2760f2427a8d99aef56414585dec9565323a995e
NZ <n.prokopyk@gmail.com>
2024-05-13 10:12:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Build a hero</h1>
+      <label htmlFor="">Name:</label>
+      <input type="text" />
+      <label htmlFor="">Age:</label>
+      <input type="number" />
+      <label htmlFor="">Height:</label>
+      <input type="text" />
+      <label htmlFor="">Superpower:</label>
+      <input type="text" />
+    </div>
+  );
+}
